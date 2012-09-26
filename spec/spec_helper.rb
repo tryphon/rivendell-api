ecad35b7af2b9b30fdb0408e52b6e24579b042e6
Alban Peignier <alban@tryphon.eu>
2012-09-26 15:52:31 +0200

    Create log directory if needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'rivendell/api'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
+FileUtils.mkdir_p "log"
+
 require "logger"
 Rivendell::API.logger = Logger.new("log/test.log")
 
